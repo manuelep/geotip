ab35d9c2e618909f1d4a45a61c26dd4cd6bc4490
manuele pesenti <manuele@valis-e.com>
2015-03-30 01:15:10 +0200

    debug demo using sqlite adapt

--- models/populate.py
+++ models/populate.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+if current.config.DEVELOPMENT and auth.user_id is None:
+    db.auth_user.truncate("CASCADE")
+
 if current.config.DEVELOPMENT:
     guest = auth.get_or_create_user(
         keys = dict(
@@ -14,18 +17,18 @@ if current.config.DEVELOPMENT:
 
 if current.config.DEVELOPMENT and auth.user_id is None:
     auth.login_user(guest)
-
-    if db(db.points.id>0).count()<20:
-        from gluon.contrib.populate import populate
-        
-        def rnd_crd_gen(_):
-            from random import uniform
-            from json import loads
-            jt = '{"type": "FeatureCollection", "features": [{"geometry": {"type": "Point", "coordinates": [%(northing)s, %(easting)s]}, "type": "Feature"}]}'
-            easting = uniform(4367840., 5736131.)
-            northing = uniform(708817., 2282650.)
-            return loads(jt % locals())
-        
-        db.points.the_geom.compute = rnd_crd_gen
-        populate(db.points, n=50, default=False, compute=True)
+    
+    db.points.truncate()
+    from gluon.contrib.populate import populate
+    
+    def rnd_crd_gen(_):
+        from random import uniform
+        from json import loads
+        jt = '{"type": "FeatureCollection", "features": [{"geometry": {"type": "Point", "coordinates": [%(northing)s, %(easting)s]}, "type": "Feature"}]}'
+        easting = uniform(4367840., 5736131.)
+        northing = uniform(708817., 2282650.)
+        return loads(jt % locals())
+    
+    db.points.the_geom.compute = rnd_crd_gen
+    populate(db.points, n=50, default=False, compute=True)
         
